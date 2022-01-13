d97b29c2cd20ee5ca86413292c350f8a16e50496
Aaron G <aaron@navarc.co>
2022-01-13 11:52:32 -0600

    got a styled table which handles data arguments

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+headings = ["Room", "Building", "location_id"]
+rows = [   ["Main Stage", "Auditorium Theatre", "1"],
+            ["Studio A", "Joffrey Tower", "2"],
+            
+]
+    
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
     return render_template("index.html")
@@ -10,6 +17,10 @@ def index():
 @app.route('/documentation', methods=['POST', 'GET'])
 def documentation():
     return render_template("documentation.html")
+
+@app.route('/locations', methods=['POST', 'GET'])
+def locations():
+    return render_template("table.html", headings=headings, rows=rows)
     
 
 if __name__ == "__main__":
