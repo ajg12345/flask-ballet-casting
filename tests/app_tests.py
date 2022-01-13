01a983606f6c722d650af326cf57ec31715b228f
Aaron G <aaron@navarc.co>
2022-01-13 17:07:04 -0600

    debug live db conn

--- tests/app_tests.py
+++ tests/app_tests.py
@@ -1,5 +1,7 @@
 from nose.tools import *
 from app import app
+from sqlalchemy import create_engine, text
+import code
 
 app.config['TESTING'] = True
 web = app.test_client()
@@ -14,3 +16,26 @@ def test_missing_pages():
 
     rv = web.get('/login', follow_redirects=True)
     assert_equal(rv.status_code, 404)
+
+
+class localdb():
+    def __init__(self):
+        self.engine = None
+        try:
+            self.engine = create_engine('mysql+pymysql://root:root@localhost:3307/flaskballetcasting')
+        except Exception as e: 
+            self.engine = create_engine('mysql+pymysql://root:root@localhost:3306/flaskballetcasting')
+            print('could not connect to localhost database because bad port' + str(e))
+
+    def get_locations(self) -> tuple:
+        """
+        Get data from the locations table, the column names and the rows in a tuple of 2 lists
+        """
+        with self.engine.connect() as conn:
+            rows = conn.execute(text("select * from locations;")) 
+            column_table = conn.execute(text("show columns in locations;")) 
+            columns = [i[0] for i in column_table] 
+        return columns, rows
+
+
+code.interact(local=locals)
