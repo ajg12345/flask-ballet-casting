fc4d18999892bc5ac960705d1a2fe6a80e797d67
Aaron G <aaron@navarc.co>
2022-01-13 21:18:01 -0600

    add table get functions to app.py

--- app.py
+++ app.py
@@ -15,11 +15,45 @@ def documentation():
 
 @app.route('/locations', methods=['POST', 'GET'])
 def locations():
-    headings, rows = ballet_data.get_locations()
+    headings, rows = ballet_data.get_table('locations')
+    return render_template("table.html", headings=headings, rows=rows)
+
+@app.route('/dancers', methods=['POST', 'GET'])
+def dancers():
+    headings, rows = ballet_data.get_table('dancers')
     return render_template("table.html", headings=headings, rows=rows)
     
+@app.route('/productions', methods=['POST', 'GET'])
+def productions():
+    headings, rows = ballet_data.get_table('productions')
+    return render_template("table.html", headings=headings, rows=rows)
+
+#ROUTES FROM HERE DOWN REQUIRE A MORE SPECIALTY QUERIES, NOT GET TABLE
+
+@app.route('/performances', methods=['POST', 'GET'])
+def performances():
+    headings, rows = ballet_data.get_performances()
+    return render_template("table.html", headings=headings, rows=rows)
+
+@app.route('/rehearsals', methods=['POST', 'GET'])
+def rehearsals():
+    headings, rows = ballet_data.get_rehearsals()
+    return render_template("table.html", headings=headings, rows=rows)
+
+@app.route('/roles', methods=['POST', 'GET'])
+def roles():
+    headings, rows = ballet_data.get_roles()
+    return render_template("table.html", headings=headings, rows=rows)
+
+@app.route('/castings', methods=['POST', 'GET'])
+def castings():
+    headings, rows = ballet_data.get_castings()
+    return render_template("table.html", headings=headings, rows=rows)
+
+@app.route('/role_conflicts', methods=['POST', 'GET'])
+def role_conflicts():
+    headings, rows = ballet_data.get_role_conflicts()
+    return render_template("table.html", headings=headings, rows=rows)
 
 if __name__ == "__main__":
-    
-
     app.run(debug=True)
